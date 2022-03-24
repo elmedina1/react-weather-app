200d22b4ec3ec02c2e7b1b3c4ca0537a3d08d4aa
elmedina1 <elmedina.sunje@gmail.com>
2022-03-24 19:39:12 +0100

    Added git link

--- src/App.js
+++ src/App.js
@@ -4,7 +4,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     
+      <h1>Weather App</h1>
+      <footer>
+        This project is created by Elmedina{' '}
+        <a
+          href="https://github.com/elmedina1/react-weather-app.git"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Git
+        </a>
+      </footer>
     </div>
   );
 }
