1bc94ed176637551eb7f78fa528d6c8bd96e0d89
elmedina1 <elmedina.sunje@gmail.com>
2022-03-24 19:59:56 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project is created by Elmedina{' '}
-        <a
-          href="https://github.com/elmedina1/react-weather-app.git"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Git
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project is created by Elmedina{' '}
+          <a
+            href="https://github.com/elmedina1/react-weather-app.git"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Git
+          </a>
+          
+        </footer>
+      </div>
     </div>
   );
 }
