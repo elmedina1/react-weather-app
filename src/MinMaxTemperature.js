c6f60c383b97ab6b20297adcd42842dd8857e306
elmedina1 <elmedina.sunje@gmail.com>
2022-04-02 16:54:36 +0200

    Update MinMaxTemperature.js

--- src/MinMaxTemperature.js
+++ src/MinMaxTemperature.js
@@ -4,36 +4,26 @@ import WeatherTemperature from './WeatherTemperature';
 
 
 export default function MinMaxTemperature(props) {
-const [units, setUnits] = useState(props.measurement);
 
-function convertToFahr(event) {
-  event.preventDefault();
-  setUnits('fahrenheit');
-}
-function convertToCel(event) {
-  event.preventDefault();
-}
+console.log('what is unit' + props.measurment);
 
-if (units === 'celsius')
- {
-    return (
-      <h4>
-        <span className="minMax">Max:</span> {props.max}{' '}
-        <sup className="unitsCurr"> °</sup>
-        <span className="minMax">Min:</span> {props.min}{' '}
-        <sup className="unitsCurr"> °</sup>
-      </h4>
-    );
-  } else {
-    return (
-      <h4>
-        <span className="minMax">Max:</span>{' '}
-        {Math.round((props.max * 9) / 5 + 32)}{' '}
-        <sup className="unitsCurr"> °</sup>
-        <span className="minMax">Min:</span>{' '}
-        {Math.round((props.min * 9) / 5 + 32)}{' '}
-        <sup className="unitsCurr"> °</sup>
-      </h4>
-    );
-  }
+if (props.measurment === 'celsius') {
+  return (
+    <h4>
+      <span className="minMax">Max:</span> {props.max}{' '}
+      <sup className="unitsCurr"> °</sup>
+      <span className="minMax">Min:</span> {props.min}{' '}
+      <sup className="unitsCurr"> °</sup>
+    </h4>
+  );
+} else {
+  return (
+    <h4>
+      <span className="minMax">Max:</span>{' '}
+      {Math.round((props.max * 9) / 5 + 32)} <sup className="unitsCurr"> °</sup>
+      <span className="minMax">Min:</span>{' '}
+      {Math.round((props.min * 9) / 5 + 32)} <sup className="unitsCurr"> °</sup>
+    </h4>
+  );
+}
 }
