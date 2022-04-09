819e42a001a489b1a558ed0f9bff999c1b07254f
elmedina1 <elmedina.sunje@gmail.com>
2022-04-09 13:21:36 +0200

    update

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -30,6 +30,9 @@ export default function WeatherForecast(props) {
                 </div>
               );
             }
+            else {
+              return(null);
+            }
           })}
         </div>
       </div>
