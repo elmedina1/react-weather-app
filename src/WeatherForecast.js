a2456fe99d35ee2f63f5e49d19be52dcf1852b60
elmedina1 <elmedina.sunje@gmail.com>
2022-04-09 12:08:52 +0200

    improvemente

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,12 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './WeatherForecast.css';
 import WeatherIcon from './WeatherIcon';
 import axios, { Axios } from 'axios';
 import WeatherForcastDay from './WeatherForcastDay';
+import { cleanup } from '@testing-library/react';
 
 export default function WeatherForecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecast, setForecast] = useState(null);
+
+  useEffect(() => {
+    setLoaded(false);
+  }, [props]);
 
   function handleResponse(response) {
     console.log('Odgovor');
@@ -20,7 +25,7 @@ export default function WeatherForecast(props) {
       <div className="WeatherForecast">
         <div className="card-group d-flex justify-content-around grupa">
           {forecast.map(function (dailyForecast, index) {
-            if (index < 5) {
+            if (index>0 && index < 6) {
               return (
                 <div key={index}>
                   <WeatherForcastDay day={dailyForecast} />
